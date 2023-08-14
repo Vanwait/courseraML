90811e19ee4082a8962d8d1c5f93dec8d0591adf
Vanwait <sergiogarciap24@gmail.com>
2023-08-14 07:53:09 +0200

    Week 2 v 2.1 - Ingeniería de variables

--- week2.py
+++ week2.py
@@ -45,7 +45,7 @@ def coste(xValues: np.array, yValues: np.array, w: np.array, b: float):
     return cost
 
 
-if __name__ == "__main__":
+def regresiónLinealMain():
     X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
     y_train = np.array([460, 232, 178])
     initial_w = np.zeros(len(X_train[0]))
@@ -59,10 +59,40 @@ if __name__ == "__main__":
         print(f"Valor real -> {y_train[i]}")
     print("--------------------------------------")
 
-
+    alpha = 5.0e-2
     X_trainN = normalizacionZscore(X_train)
     w, b = descensoGradiente(initial_w, initial_b, iterations, X_trainN, y_train, alpha, descenso)
     print(f"El valor de w es {w}, el valor de b es {b}")
     for i in range(len(X_train)):
         print(f"Predicción -> {np.dot(w, X_trainN[i]) + b}")
         print(f"Valor real -> {y_train[i]}")
+
+
+def mostrarValues(x, X, y, w, b, feature):
+    if feature:
+        plt.title("feature engineering")
+    else:
+        plt.title("no feature engineering")
+    plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("no feature engineering")
+    plt.plot(x,X@w + b, label="Predicted Value");  plt.xlabel("X"); plt.ylabel("y"); plt.legend(); plt.show()
+
+
+def ingenieriaCaracteristicas():
+    x = np.arange(0, 20, 1)
+    y = x**2
+    #X = normalizacionZscore(x.reshape(-1, 1))
+    #X = x**2
+    #X = normalizacionZscore(X.reshape(-1, 1))
+    #X = X.reshape(-1, 1)
+    X = np.c_[x, x**2, x**3]
+    X = normalizacionZscore(X)
+
+    w, b = descensoGradiente(np.zeros(X.shape[1]), 0, 10000, X, y, 1e-1, descenso) 
+    print(f"{w} // {b}") ##Viendo los valores de w, al tener más valor para x^2, esta variable tiene más relevancia en este caso, lo cual es normal dado que la función es y = x^2
+    mostrarValues(x, X, y, w, b, True)
+    
+
+if __name__ == "__main__":
+    #regresiónLinealMain()
+    ingenieriaCaracteristicas()
+    
